c6920bf16019694449931fd8a21c15f5891d4dc8
divyansraj <divyanshu10245@gmail.com>
2024-06-17 18:16:43 +0530

    deploying v2

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,32 @@
-require("dotenv").config()
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const fileUpload = require("express-fileupload")
+const fileUpload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
-
 
 const app = express();
 
-//middlewares
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(
-  cors({
-    origin: ["http://localhost:5174", "http://localhost:5173"],
-  })
-);
+
+// Set CORS options
+const corsOptions = {
+  origin: [
+    "http://localhost:5174",
+    "http://localhost:5173",
+    "https://foodio-app-yzyz.vercel.app",
+  ],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
+// Use CORS middleware
+app.use(cors(corsOptions));
+
 // Use Helmet with CSP middleware
 app.use(
   helmet.contentSecurityPolicy({
@@ -32,37 +40,28 @@ app.use(
 // Morgan middleware
 app.use(morgan("tiny"));
 
-//cookie and file middlewares
+// Cookie and file middlewares
 app.use(cookieParser());
 app.use(
   fileUpload({
     useTempFiles: true,
-    tempFileDir: path.join("/tmp"), // Use the writable directory provided by the platform
+    tempFileDir: path.join("/tmp"),
   })
 );
 
-//importing all the routes here
+// Importing all the routes
+const food = require("./routes/food");
+const user = require("./routes/user");
+const cart = require("./routes/cart");
+const order = require("./routes/order");
 
-const food= require('./routes/food')
-const user =require('./routes/user')
-const cart = require('./routes/cart')
-const order = require('./routes/order')
-
-app.use('/api/food',food);
-app.use('/api/user',user);
-app.use('/api/cart',cart);
-app.use('/api/order',order);
-
-
-
+app.use("/api/food", food);
+app.use("/api/user", user);
+app.use("/api/cart", cart);
+app.use("/api/order", order);
 
 app.get("/", (req, res) => {
   res.send("Hello Server");
 });
-
-
-
-
-//export app.js
 
 module.exports = app;
--- frontend/src/utils/constants.jsx
+++ frontend/src/utils/constants.jsx
@@ -1,2 +1,2 @@
 //export const myURL = "http://localhost:4000";
-export const myURL = "https://foodio-server.vercel.app/";
+export const myURL = "https://foodio-server.vercel.app";
