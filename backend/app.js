b5c850f7ff79590a53068ef2221fc195fcbad2dc
divyansraj <divyanshu10245@gmail.com>
2024-06-17 16:09:23 +0530

    deplyed v3

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,18 @@
 require("dotenv").config()
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const path = require("path");
-
 const helmet = require("helmet");
 const morgan = require("morgan");
-
 const fileUpload = require("express-fileupload")
 const cookieParser = require("cookie-parser");
 
+
+const app = express();
+
 //middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(
   cors({
---
+++ backend/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const cloudinary = require('cloudinary');
+const cors = require("cors");
+const app= require('./app')
+
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5174", "http://localhost:5173"],
+  })
+);///frontend url admin url
+
+//connecting to database
+const connectDB = require('./config/db');
+connectDB();
+
+//configuring cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_APIKEY,
+  api_secret: process.env.CLOUD_API_SECRETKEY,
+});
+
+app.listen(port,()=>{
+    console.log(`server is running at http://localhost:${port}`)
+})
+
+// mongodb+srv://divyanshuww:WVGmGjGPWEx9h6x5@cluster0.hi2pr4k.mongodb.net/?
